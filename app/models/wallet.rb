c70cfba26f005e81e6c7feabf8aa4998bdafa650
nandang <net.nandang@gmail.com>
2024-11-28 14:20:56 +0700

    add activerecord associations, validations

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,13 @@
 class Wallet < ApplicationRecord
   belongs_to :entity, polymorphic: true
+
+  has_many :source_transactions, class_name: "Transaction", foreign_key: :source_wallet_id
+  has_many :target_transactions, class_name: "Transaction", foreign_key: :target_wallet_id
+
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
+
+  # helper method for calculate total transactions
+  def total_transactions
+    source_transactions.count + target_transactions.count
+  end
 end
